035e3048b877ad945ccb2f60ed63827898b230ee
Emanuele Casseri <emanuele.casseri@gmail.com>
2023-03-22 16:32:30 +0100

    aggiunte icone nav bar e carosello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { faShrimp } from '@fortawesome/free-solid-svg-icons';
+import { faRectangleList } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cibando';
+
+  faShrimp = faShrimp;
+  faRectangleList = faRectangleList;
+  faHome = faHome;
+
+  images = [
+    // {id:  1,
+    //   label: 'Spaghetti  al pomodoro',
+    // },
+    // {id:  2,
+    //   label: 'Tagliata di manzo',
+    // },
+    // {id:  1,
+    //   label: 'Tiramisú',
+    // }
+  ]
+
+  percorso = "../assets/images/carousel-";
+
+  colore = 'red';
+
+
 }
+
